12469be0a08a2c115989638eac6a50c9cdb39b74
leopeng1995 <leopeng1995@hotmail.com>
2024-10-29 23:29:09 +0800

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# QwerGPT
+
+A Lightweight LLM Framework
+
+### Quick Start
+
+```
+cp .env.example .env
+```
---
+++ qwergpt/llm/__init__.py
@@ -0,0 +1,12 @@
+from .zhipu import ZhipuLLM
+from .tonyi import TongyiLLM
+from .ollama import OllamaLLM
+from .deepseek import DeepSeekLLM
+
+
+__all__ = [
+    'ZhipuLLM',
+    'TongyiLLM',
+    'OllamaLLM',
+    'DeepSeekLLM',
+]
